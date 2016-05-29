d769715e78628d764b19d0e493e4aece661e1103
Tomáš Kubala <kubala.tomas@hotmail.com>
2016-05-29 22:45:09 +0200

    First upload

---
+++ MP3 Converter/Converter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+
+namespace MP3_Converter
+{
+    class Converter
+    {
+        public static void WaveToMP3(string inputFile)
+        {
+            string outputFile = (string)inputFile.Clone();
+
+            int count = 0;
+            for (int i = outputFile.Length; i > 0; i--)
+            {
+                if (outputFile[i-1] == '.')
+                    break;
+
+                count++;
+            }
+            outputFile = outputFile.Remove(outputFile.Length - count, count);
+            outputFile += "mp3";
+            File.Create(outputFile).Dispose();
+            ProcessStartInfo lame = new ProcessStartInfo();
+            lame.FileName = "lame\\lamess.exe";
+            lame.Arguments = "-V2 " + inputFile + " " + outputFile;
+            lame.WindowStyle = ProcessWindowStyle.Hidden;
+            Process p;
+            try
+            {
+                p = Process.Start(lame);
+            }
+            catch (System.ComponentModel.Win32Exception ex)
+            {
+                throw ex;
+            }
+            p.WaitForExit();
+        } 
+    }
+
+    class Arguments
+    {
+
+    }
+}
---
+++ MP3 Converter/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MP3_Converter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        OpenFileDialog ofd_ = new OpenFileDialog();
+        public MainWindow()
+        {
+            InitializeComponent();
+            arguments_cb.Items.Clear();
+            arguments_cb.Items.Add("64 kbps");
+            arguments_cb.Items.Add("128 kbps");
+            arguments_cb.Items.Add("192 kbps");
+            arguments_cb.Items.Add("256 kbps");
+            arguments_cb.Items.Add("320 kbps");
+            arguments_cb.SelectedIndex = 1;
+            ofd_.Filter = "Music files (*.mp3, *.wma, *.wav) | *.mp3; *.wma; *.wav";
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            ofd_.ShowDialog();
+            path_tb.Text = ofd_.FileName;
+        }
+
+        private void radioButtonChecker(object sender, RoutedEventArgs e)
+        {
+            if ((bool)vbr_rb.IsChecked)
+            {
+                cbr_rb.IsChecked = false;
+                vbr_rb.IsChecked = true;
+                arguments_cb.Items.Clear();
+                arguments_cb.Items.Add("V1");
+                arguments_cb.Items.Add("V2");
+                arguments_cb.Items.Add("V3");
+                arguments_cb.Items.Add("V4");
+                arguments_cb.Items.Add("V5");
+                arguments_cb.SelectedIndex = 1;
+            }
+            else
+            {
+                cbr_rb.IsChecked = true;
+                vbr_rb.IsChecked = false;
+                arguments_cb.Items.Clear();
+                arguments_cb.Items.Add("64 kbps");
+                arguments_cb.Items.Add("128 kbps");
+                arguments_cb.Items.Add("192 kbps");
+                arguments_cb.Items.Add("256 kbps");
+                arguments_cb.Items.Add("320 kbps");
+                arguments_cb.SelectedIndex = 1;
+            }
+        }
+
+        private void removeFile_b_Click(object sender, RoutedEventArgs e)
+        {
+            path_tb.Text = "Empty";
+        }
+
+        private void convert_b_Click(object sender, RoutedEventArgs e)
+        {
+            Converter.WaveToMP3(path_tb.Text);
+        }
+    }
+}
